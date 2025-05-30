b8b189aa90a8868675f5c5876f276d3d32dd7e0a
HurricaneLily <jostin27022023@gmail.com>
2025-05-30 18:40:08 +0400

    Notification (click on link to unavailable page)

--- src/components/blocks/projects.jsx
+++ src/components/blocks/projects.jsx
@@ -2,11 +2,19 @@ import { useTranslation } from 'react-i18next';
 import { projects } from "../../data/projects";
 import SectionTitle from "../ui/section-title";
 import ProjectCard from '../ui/project-card';
+import CustomToaster from "../ui/custom-toaster";
 import { useFont } from '../../hooks/hooks';
+import toast from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+import { GoArrowRight } from "react-icons/go";
 
 function Projects () {
     const {t, i18n} = useTranslation();
     const font = useFont();
+
+    const notifyUnavailable = () => {
+        toast.custom((tst) => <CustomToaster tst={tst} text={t("global.notifyUnavailable")} />)
+    };
 
     return (
         <section className='py-16 md:py-20 lg:py-24 font-montserrat' id="projects">
@@ -18,6 +26,25 @@ function Projects () {
                     </li>
                 )}
             </ul>
+                <a
+                    href="#"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        notifyUnavailable();
+                    }}
+                    className="flex sm:justify-end items-center group mt-8 sm:mt-10"
+                >
+                    <span className="mr-1.5 text-xs sm:text-sm lg:text-base font-medium text-[#311409]/80 group-hover:text-[#311409] dark:text-[#959595] dark:group-hover:text-[#f5f5f5] transition-colors duration-300">
+                        {t("global.showAll")}
+                    </span>
+                    <span className="group-hover:translate-x-1 transition-transform duration-300">
+                        <GoArrowRight className="h-3.5 w-3.5 sm:h-4 sm:w-4 text-[#311409]/80 group-hover:text-[#311409] dark:text-[#959595] dark:group-hover:text-[#f5f5f5] transition-colors duration-300" />
+                    </span>
+                </a>
+                <Toaster
+                    position="bottom-right"
+                    reverseOrder={true}
+                />
         </section>
     )
 }
---
+++ src/components/ui/custom-toaster.jsx
@@ -0,0 +1,20 @@
+import { toast } from "react-hot-toast";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+function CustomToaster ({tst, text}) {
+    return (
+        <div 
+            className="rounded-xl flex items-center gap-3 px-5 py-7 cursor-pointer bg-[#ff000020] dark:bg-[#111111] border border-[#ff0000]/40 dark:border-[#00eeff]/50 text-[#ff0000]/40 dark:text-[#00eeff]/50 hover:text-[#ff0000]/70 dark:hover:text-[#00eeff]/70"
+            onClick={() => toast.dismiss(tst.id)}
+        >
+            <button
+                className="w-4 h-4 border-0 bg-transparent p-0 flex items-center justify-center focus:text-[#ff0000]/70 dark:focus:text-[#00eeff]/70 focus:outline-none"
+            >
+                <AiFillCloseCircle />
+            </button>
+            <p className="text-xs sm:text-sm lg:text-base text-[#311409]/80 dark:text-white/80">{text}</p>
+        </div>
+    )
+}
+
+export default CustomToaster;
